d3092cfd8c38be2ae3f6daffed7b2f8d26cebe90
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-08-02 13:11:45 -0700

    Complete button and random color

--- script.js
+++ script.js
@@ -2,19 +2,55 @@ const bodyElement = document.querySelector('body');
 const containerDiv = document.createElement('div');
     containerDiv.classList.add('container');
 
-for(let j = 0; j < 16; j++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('column');
-    containerDiv.appendChild(rowDiv);
+let userGridInput = 16;
 
-    for(let i = 0; i < 16; i++){  
-        const div = document.createElement('div');
-        div.classList.add(`grid`);
-        div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'red';
-        });
-        rowDiv.appendChild(div);
+function promptMessage() {
+    return Number(prompt('Set grid size - minimum 16, maximum 100.'));
+};
+
+const button = document.createElement('button');
+    button.textContent = 'New grid';
+    button.addEventListener('click', () => {
+        userGridInput = promptMessage();
+        while(userGridInput < 16 || userGridInput > 100 || isNaN(userGridInput)){
+            alert('Invalid entry, try again');
+            userGridInput = promptMessage();
+        }
+        const divColumns = document.querySelectorAll('.column');
+        divColumns.forEach(column => column.remove());
+        createGrid(userGridInput);
+    });
+    bodyElement.appendChild(button);
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    let alpha = 1.0;
+    return `rgb(${r}, ${g}, ${b}, ${alpha})`;
+}
+
+function createGrid(userGridInput) {
+    for(let j = 0; j < userGridInput; j++){
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('column');
+        containerDiv.appendChild(rowDiv);
+    
+        for(let i = 0; i < userGridInput; i++){  
+            const div = document.createElement('div');
+            div.classList.add(`grid${j}${i}`);
+            div.style.flex = '1';
+            div.style.margin = '0';
+            div.style.padding = '1';
+            div.style.backgroundColor = 'lightblue';
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = getRandomColor();
+            });
+            rowDiv.appendChild(div);
+        }
     }
 }
 
 bodyElement.appendChild(containerDiv);
+
+createGrid(userGridInput);
